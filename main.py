a0d52035d7820505e30ae2db994ebc24dd1b963e
icyclestick <icebebe111@gmail.com>
2025-07-16 18:36:04 +0800

    feat: sys arguments

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
+
 from stats import get_book_text, count_words, count_letters, pretty_report
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    book_string = get_book_text("./books/frankenstein.txt")
-    print("Analyzing book found at books/frankenstein.txt...")
+    book_string = get_book_text(sys.argv[1])
+    print(f"Analyzing book found at {sys.argv[1]}") 
     num_of_words = count_words(book_string)
     print("----------- Word Count ----------")
     print(f"Found {num_of_words} total words")
